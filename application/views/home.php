c85176afebb7cc625d83cc68497099307ec8cd8e
brex <brex@gmail.com>
2019-03-19 17:48:35 +0800

    My GIT commit

---
+++ application/views/home.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+<body>
+	<h1> hello world! </h1>
+</body>
+<script type="text/javascript" href="assets/js/javascript.js"></script>
+</html>
